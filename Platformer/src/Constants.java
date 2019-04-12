b2893c2d62bd95530dd4180e437e6750f06adf2e
GitBoi37 <45974904+GitBoi37@users.noreply.github.com>
2019-04-12 10:25:03 -0700

    Update Constants.java

--- Platformer/src/Constants.java
+++ Platformer/src/Constants.java
@@ -1,5 +1,10 @@
 
 public class Constants {
+    //dot constants
+    public final int DOTDAMAGE = 1;
+    public final int DOTSPEED = 1;
+    public final int DOTVALUE = 1;
+    public final int DOTPROXIM = 15;
     public final int DELAY = 5;
     public final int[] speedRange = {100,150};
     public final int JUMP = 250;
@@ -10,7 +15,7 @@ public class Constants {
     public final int SPAWNBUFFER = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight()/4;
     public final int DEFAULTWIDTH = 100;
     public final int DEFAULTHEIGHT = 10;
-	public final int SPAWNPLATFORMDELAY = 100;
-	public int testint = 0;
+    public final int SPAWNPLATFORMDELAY = 100;
+    public int testint = 0;
     public Constants() {}
 }
